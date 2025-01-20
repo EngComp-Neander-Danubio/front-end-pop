5a4a35d81f658abec1b750b500780d577c60a5f9
NeanderEngComp-Neander-Danubio <neander.eng@alu.ufc.br>
2025-01-20 13:45:38 -0300

    consulta a api para a page de módulos

--- src/components/listagem-de-modulos/ToListModulosContent.tsx
+++ src/components/listagem-de-modulos/ToListModulosContent.tsx
@@ -1,12 +1,48 @@
 import { Flex, Icon } from '@chakra-ui/react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { CardModulo } from '../componentesModulos/CardModulo';
 import { IoBookSharp } from 'react-icons/io5';
 import { FiVideo } from 'react-icons/fi';
 import { FaFileAlt } from 'react-icons/fa';
 import { RxText } from "react-icons/rx";
+import api from '../../services/api';
+type CadastroForm = {
+  title: string;
+    description: string;
+    reference?: string;
+    system?: string;
+    assunto?: string;
+    descriptionAdd?: string;
+    pdfFilePath?: File,
+    createdBy?: string,
+    sectorContactName?: string,
+    sectorContactEmail?: string,
+    sectorContactPhone?: string,
+    keywords?: string[];
+    createdAt?: Date;
+    updatedAt?: Date;
+    deletedAt?: Date;
+
+}
 // lista as solicitacoes da OPM no que se refere ao posto de serviço
 export const ToListModulosContent: React.FC = () => {
+  const [tutorial, setTutorial] = useState<CadastroForm[]>([]);
+    const loadTutorialFromToBackend = async () => {
+      try {
+        const response = await api.get<CadastroForm[]>(`/pops`);
+        setTutorial(response.data);
+
+        } catch (err) {
+          if (err instanceof Error) {
+            console.error(`Erro ao carregar postos: ${err.message}`);
+          } else {
+            console.error('Erro desconhecido ao carregar postos:', err);
+          }
+        }
+      };
+    useEffect(()=>{
+      loadTutorialFromToBackend()
+    },[])
 
   return (
     <>
@@ -14,15 +50,15 @@ export const ToListModulosContent: React.FC = () => {
       <Flex  flexDirection={'row'} gap={10}>
 
         <CardModulo icon={<Icon as={IoBookSharp} boxSize={7} color={'#fff'}/>}
-        title={'Todos'} body='' textsFooter={'Todos os Treinamentos'} numberTutorial={10} />
+        title={'Todos'} body='' textsFooter={'Todos os Treinamentos'} numberTutorial={tutorial.length} />
         <CardModulo icon={<Icon as={FiVideo} boxSize={7} color={'#fff'} />}
-        title={'Vídeos'} body='' textsFooter={'Treinamentos com Vídeos'} numberTutorial={5} />
+        title={'Vídeos'} body='' textsFooter={'Treinamentos com Vídeos'} numberTutorial={tutorial.length} />
         </Flex>
         <Flex  flexDirection={'row'} gap={10}>
         <CardModulo icon={<Icon as={FaFileAlt} boxSize={7} color={'#fff'} />}
-        title={'Documentos'} body='' textsFooter={'Treinamentos com Documentos'} numberTutorial={8} />
+        title={'Documentos'} body='' textsFooter={'Treinamentos com Documentos'} numberTutorial={tutorial.length} />
         <CardModulo icon={<Icon as={RxText} boxSize={7} color={'#fff'}  />}
-        title={'Textos'} body='' textsFooter={'Treinamentos com textos'} numberTutorial={8} />
+        title={'Textos'} body='' textsFooter={'Treinamentos com textos'} numberTutorial={tutorial.length} />
         </Flex>
 
       </Flex>
